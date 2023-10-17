82559bcb56267d2a570786c80ede15c36c0bd1d0
Moad Hamousti <118165767+moadhamousti@users.noreply.github.com>
2023-10-17 12:11:03 +0100

    Add files via upload

---
+++ scripts/motion.js
@@ -0,0 +1,38 @@
+
+TweenMax.from(".intro", 1, {
+	opacity: 0,
+	x: 30,
+    delay: .3,
+	ease: Expo.easeInOut
+})
+
+TweenMax.from(".descrip", 1, {
+	opacity: 0,
+	x: -30,
+    delay: .3,
+	ease: Expo.easeInOut
+})
+
+TweenMax.from(".logo-main", 1, {
+	opacity: 0,
+	x: -30,
+    delay: .3,
+	ease: Expo.easeInOut
+})
+
+
+TweenMax.from(".nav", 1, {
+	opacity: 0,
+	x: -30,
+    delay: .3,
+	ease: Expo.easeInOut
+})
+
+
+TweenMax.from(".nav-button", 1, {
+	opacity: 0,
+	x: -30,
+    delay: 0.3,
+	ease: Expo.easeInOut
+})
+
